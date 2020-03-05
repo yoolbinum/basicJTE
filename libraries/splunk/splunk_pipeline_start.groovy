490754e6fc8c0acb5f91c1e4bddcd7c31b58e219
yoolbinum <yoolbin.um@gmail.com>
2020-03-05 18:50:55 -0500

    created end file

--- libraries/splunk/splunk_pipeline_start.groovy
+++ libraries/splunk/splunk_pipeline_start.groovy
@@ -6,3 +6,8 @@ void call(context){
 void before(context){
     println "Splunk: running before the ${context.library} library's ${context.step} step"
 }
+
+@AfterStep
+void after(context){
+    println "Splunk: running after the ${context.library} library's ${context.step} step"
+}
