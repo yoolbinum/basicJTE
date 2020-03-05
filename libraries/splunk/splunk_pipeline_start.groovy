676b3217feb0b1ddb4db8f93ec4dbe2732cf2a18
yoolbinum <yoolbin.um@gmail.com>
2020-03-05 18:12:37 -0500

    rollback

--- libraries/splunk/splunk_pipeline_start.groovy
+++ libraries/splunk/splunk_pipeline_start.groovy
@@ -7,7 +7,7 @@ void before(context){
     println "Splunk: running before the ${context.library} library's ${context.step} step"
 }
 
-@AfterStep({ context.step.equals("static_code_analysis") })
+@AfterStep
 void after(context){
     println "Splunk: running after the ${context.library} library's ${context.step} step"
 }
