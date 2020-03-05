690c430594689538e631ae89a2145da95b2b3cda
yoolbinum <yoolbin.um@gmail.com>
2020-03-05 17:52:47 -0500

    splunk: restricting hook to afterstep

--- libraries/splunk/splunk_pipeline_start.groovy
+++ libraries/splunk/splunk_pipeline_start.groovy
@@ -6,3 +6,13 @@ void call(context){
 void before(context){
     println "Splunk: running before the ${context.library} library's ${context.step} step"
 }
+
+@AfterStep({ context.step in config.afterSteps })
+void after(context){
+    println "Splunk: running after the ${context.library} library's ${context.step} step"
+}
+
+@CleanUp
+void call(context){
+    println "Splunk: end of the pipeline!"
+}
