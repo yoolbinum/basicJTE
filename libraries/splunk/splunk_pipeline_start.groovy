6edc0576751677470084ca61ab151905e568b9fd
yoolbinum <yoolbin.um@gmail.com>
2020-03-05 18:53:04 -0500

    Adding hook to after splunk

--- libraries/splunk/splunk_pipeline_start.groovy
+++ libraries/splunk/splunk_pipeline_start.groovy
@@ -7,7 +7,7 @@ void before(context){
     println "Splunk: running before the ${context.library} library's ${context.step} step"
 }
 
-@AfterStep
+@AfterStep({ context.step in config.afterSteps })
 void after(context){
     println "Splunk: running after the ${context.library} library's ${context.step} step"
 }
